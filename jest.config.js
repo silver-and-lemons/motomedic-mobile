936e65bd7db6a46a35ec590638c1d225e73c26b0
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-08-11 14:03:35 +0800

    fix(jest): switch to jest-expo preset with expo and async-storage mocks

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 /** @type {import('jest').Config} */
+const expoPreset = require('jest-expo/jest-preset');
+
 module.exports = {
-  preset: '@react-native/jest-preset',
-  setupFiles: ['./jest.setup.ts'],
-  transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
-  },
+  preset: 'jest-expo',
+  setupFiles: [...expoPreset.setupFiles, './jest.setup.ts'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-native-community|@react-navigation|expo|@expo|expo-router|@hookform|@tanstack|react-hook-form|nativewind|react-native-css-interop|zod|zustand|@testing-library|react-native-reanimated|react-native-gesture-handler|react-native-screens|react-native-safe-area-context|react-native-worklets|react-native-web|expo-modules-core|expo-constants|expo-status-bar|expo-font|expo-linking|expo-device|class-variance-authority|clsx|tailwind-merge)/)',
+    ...expoPreset.transformIgnorePatterns,
+    'node_modules/(?!(react-native|@react-native|@react-native-community|@react-navigation|expo|@expo|expo-router|@hookform|@tanstack|react-hook-form|nativewind|react-native-css-interop|zod|zustand|@testing-library|react-native-reanimated|react-native-gesture-handler|react-native-screens|react-native-safe-area-context|react-native-worklets|react-native-web|expo-modules-core|expo-constants|expo-secure-store|expo-status-bar|expo-font|expo-linking|expo-device|class-variance-authority|clsx|tailwind-merge)/)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^@react-native-async-storage/async-storage$':
+      '@react-native-async-storage/async-storage/jest/async-storage-mock',
   },
 };
