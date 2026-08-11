cf74cf596490fb2d2010c5fe401d9292260e3bd4
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2026-08-11 11:54:08 +0800

    feat: Add DashboardScreen to render DiagnosticHistoryContainer

---
+++ src/app/diagnostic-hist.tsx
@@ -0,0 +1,5 @@
+import DiagnosticHistoryContainer from '../features/diagnostic-hist/containers/DiagnosticHistoryContainer';
+
+export default function DashboardScreen() {
+  return <DiagnosticHistoryContainer />;
+}
